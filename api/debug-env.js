9b41fea4e32a69e7d74bab3948b34b5b783007f6
Maik Malletschek <maik@Mac.fritz.box>
2026-02-28 17:28:08 +0100

    Temp: debug secret

--- api/debug-env.js
+++ api/debug-env.js
@@ -5,5 +5,8 @@ export default function handler() {
     strava_client_id_set: !!process.env.STRAVA_CLIENT_ID,
     strava_client_id_length: process.env.STRAVA_CLIENT_ID?.length ?? 0,
     strava_client_id_value: process.env.STRAVA_CLIENT_ID ?? 'NICHT GESETZT',
+    strava_client_secret_set: !!process.env.STRAVA_CLIENT_SECRET,
+    strava_client_secret_length: process.env.STRAVA_CLIENT_SECRET?.length ?? 0,
+    strava_client_secret_last3: process.env.STRAVA_CLIENT_SECRET?.slice(-3) ?? 'NICHT GESETZT',
   }), { headers: { 'content-type': 'application/json' } })
 }
